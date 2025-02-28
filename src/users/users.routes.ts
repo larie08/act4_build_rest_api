d6b3adc3ecccfaf323ad1df60759ac036815a830
unknown <YY442@ccs.local>
2025-02-28 05:28:13 -0800

    tiwas balay

--- src/users/users.routes.ts
+++ src/users/users.routes.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from "express"
 import { UnitUser, User } from "./user.interface"
-import { StatusCodes } from "http-status-codes"\
+import { StatusCodes } from "http-status-codes"
 import * as database from "./user.database"
 
 export const userRouter = express.Router()
@@ -19,4 +19,60 @@ userRouter.get("/users", async (req : Request, res : Response) => {
    }
 })
 
-userRouter.get("")
+userRouter.get("/user/:id", async (req : Request, res : Response) =>{
+    try{
+        const user : UnitUser = await database.findOne(req.params.id)
+
+        if (!user){
+            return res.status(StatusCodes.NOT_FOUND).json({error : `User not found!`})
+        }
+
+        return res.status(StatusCodes.OK).json({user})
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+    }
+})
+
+userRouter.post("/register", async (req : Request, res : Response) =>{
+    try{
+        const { username, email, password } = req.body
+
+        if (!username || !email || !password){
+            return res.status(StatusCodes.BAD_REQUEST).json({error : `Please provide all the required parameters..`})
+        }
+
+        const user = await database.findByEmail(email)
+
+        if (user) {
+            return res.status(StatusCodes.BAD_REQUEST).json({error : `This email has already been registered..`})
+        }
+
+        const newUser = await database.create(req.body)
+
+        return res.status(StatusCodes.CREATED).json({newUser})
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+    }
+})
+
+userRouter.post("/login", async (req : Request, res : Response) => {
+    try{
+        const {email, password} = req.body
+
+        if (!email || !password) {
+            return res.status(StatusCodes.BAD_REQUEST).json({error : `Please provide all the required parameters..`})
+        }  
+
+        const user = await database.findByEmail(email)
+
+        if (!user){
+            return res.status(StatusCodes.NOT_FOUND).json({error : `No user exists with the email provided..`})
+        }
+
+        const comparePassword = await database.comparePassword(email, password)
+
+        if (!comparePassword){
+            return res.status(StatusCodes.BAD_REQUEST).json({error : `Incorrect Password!`})
+        }
+    }
+})
